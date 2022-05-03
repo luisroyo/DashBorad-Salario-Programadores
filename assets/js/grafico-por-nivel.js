b913c2fa38d09945161c1d8e391c0d8febb8dbd6
Luis Eduardo <luisroyo25@gmail.com>
2022-05-02 21:25:20 -0300

    Aplicando Bootstrap e simplificando o dashboard

--- assets/js/grafico-por-nivel.js
+++ assets/js/grafico-por-nivel.js
@@ -1,29 +1,23 @@
-google.charts.load('current', {'packages':['corechart','geochart']});
-google.charts.setOnLoadCallback(drawVisualization,);
-function drawVisualization() {
-    // Some raw data (not necessarily accurate)
+google.charts.load('current', { packages: ['corechart'] });
+google.charts.setOnLoadCallback(drawChart); 
+function drawChart() {
+
     var data = google.visualization.arrayToDataTable([
-      ['País' , '         Média salárial'],
-      ['PORTUGAL',               12153.83],
-      ['CANADA',                 29806.00],
-      ['ALEMANHA',               29857.64],
-      ['IRLANDA',                26389.33],      
-      ['JAPÃO',                  18786.21],
-      ['ANGOLA',                 18786.14],
-      ['EUA',                    33000.33],
-      ['HOLANDA',                31667.17],
-      ['INGLATERRA',             26750.50],
-      ['PAÍSES BAIXO',           34000.40]
+      ['Task', 'Hours per Day'],
+      ['Júnior',     31.94],
+      ['Pleno',      31.05],
+      ['Sênior',  25.99],
+      ['Estágio', 8.10],
+      ['Outros', 2.93]
     ]);
 
-    var options = {       
-      title : 'Média salárial dos 10 países que mais participaram ',
-      vAxis: {title: 'Salário'},
-      hAxis: {title: 'Países'},
-      seriesType: 'bars',
-      series: {5: {type: 'line'}}
+    var options = {
+      title: 'Dos Participantes',
+      width:'100%',
+      height:'400px'
     };
 
-    var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
     chart.draw(data, options);
   }
---
+++ assets/js/grafico-por-paisl.js
@@ -0,0 +1,29 @@
+google.charts.load('current', {'packages':['corechart','geochart']});
+google.charts.setOnLoadCallback(drawVisualization,);
+function drawVisualization() {
+    // Some raw data (not necessarily accurate)
+    var data = google.visualization.arrayToDataTable([
+      ['País' , '         Média salárial'],
+      ['PORTUGAL',               12153.83],
+      ['CANADA',                 29806.00],
+      ['ALEMANHA',               29857.64],
+      ['IRLANDA',                26389.33],      
+      ['JAPÃO',                  18786.21],
+      ['ANGOLA',                 18786.14],
+      ['EUA',                    33000.33],
+      ['HOLANDA',                31667.17],
+      ['INGLATERRA',             26750.50],
+      ['PAÍSES BAIXO',           34000.40]
+    ]);
+
+    var options = {       
+      title : 'Média salárial dos 10 países que mais participaram ',
+      vAxis: {title: 'Salário'},
+      hAxis: {title: 'Países'},
+      seriesType: 'bars',
+      series: {5: {type: 'line'}}
+    };
+
+    var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
